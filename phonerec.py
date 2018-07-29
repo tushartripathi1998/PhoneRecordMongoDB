b454748428f6cee3bbaf8daf0216ac0915e6e551
tushartripathi1998 <thetushartripathi1998@gmail.com>
2018-07-29 18:14:04 +0530

    completed crud

--- phonerec.py
+++ phonerec.py
@@ -8,7 +8,7 @@ db=client['rec_database']                                                       
 collection = db.rec_database                                                    #basically to show the type of collection
 posts=db.posts
 
-@app.route("/")
+@app.route("/enter_data")
 def enterval() -> 'html' :
     return render_template('enter_data.html')
 
@@ -24,11 +24,68 @@ def add() -> 'html' :
 
     return render_template('feed_data.html',lsp=lsp)
 
-@app.route("/stats",methods=['GET','POST'])
+@app.route("/",methods=['GET'])
 def show_stats()-> 'html' :
     ls=[]
     for post in posts.find():
         ls.append(post)
     return render_template('show_stats.html',ls=ls)
+
+@app.route("/del",methods=['GET','POST'])
+def del_data() -> 'html' :
+    dl=request.form
+    #posts.remove({dl})
+    #return render_template('show_stats.html',ls=dl)
+    return render_template('chk.html',ps=dl)
+
+@app.route("/delete",methods=['GET','POST'])
+def deleted() -> 'html' :
+    pl=request.form
+    posts.remove({"valname":pl['todel1']}) #{$or: [{"valname":pl['todel1']} , {"valsize":pl['todel2']} , {"valprice":pl['todel3']}]} )
+    return render_template('chk.html',delete=pl)
+
+@app.route("/checking")
+def checking() -> 'html' :
+    p=request.form['h']
+    return render_template('check.html',d=p)
+
+'''@app.route("/change",methods=['GET','POST'])
+def change() -> 'html' :
+    pl=request.form
+    render_template('entr_data.html',n1=,n2=,n3=)
+'''
+
+@app.route("/enter_updated_data")
+def enter_updated_data() -> 'html' :
+    ls=[]
+    for post in posts.find():
+        ls.append(post)
+    return render_template("update.html",ls=ls)
+
+@app.route("/update",methods=['GET','POST'])
+def update() -> 'html' :
+    pr1=request.form['browser']
+    pr2=request.form['toup2']
+    posts.update_one(
+        {"valname" : pr1},
+        {
+        "$set":{
+            "valprice":pr2
+        }
+        }
+    )
+    ls=[]
+    for post in posts.find():
+        ls.append(post)
+    return render_template('show_stats.html',ls=ls)
+
+@app.route("/see",methods=['GET','POST'])
+def see() -> 'html' :
+    return render_template('search.html')
+
+@app.route("/search",methods=['GET','POST'])
+def search() ->'html' :
+    ab=request.form["say"]
+    return render_template('check.html',d=ab)
 
 app.run(debug=True)
